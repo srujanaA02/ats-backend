e6c76a585ff9d80ef24101bda4401712c829c1fb
srujanaA02 <23mh1a4902@acoe.edu.in>
2025-12-20 19:55:49 +0530

    Fix: Correct ApplicationViewSet class definition syntax error

--- ats/views.py
+++ ats/views.py
@@ -68,7 +68,7 @@ class JobViewSet(viewsets.ModelViewSet):
 # APPLICATION VIEWSET
 # ======================================================
 70
-(viewsets.ModelViewSet):
+class ApplicationViewSet(viewsets.ModelViewSet):
     queryset = Application.objects.all()
     serializer_class = ApplicationSerializer
     permission_classes = [IsAuthenticated]
